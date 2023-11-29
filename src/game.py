bc30a221eefb07ff9c3a9e3c437f47fb7f88481e
StratenyNinja <erik.stvrtecky2000@gmail.com>
2023-11-29 10:52:27 +0100

    Add method to check neighbors

--- src/game.py
+++ src/game.py
@@ -3,8 +3,23 @@ import random
 
 class Game:
     def __init__(self, rows: int, cols: int) -> None:
+        self.rows = rows
+        self.cols = cols
         self.grid = [[random.choice([0, 1]) for col in range(cols)] for row in range(rows)]
 
     def print_grid(self):
         for row in self.grid:
             print(row)
+
+    def check_neighbors(self, x, y):
+        neighbors = 0
+
+        for rowIndex in range(-1, 2):
+            for colIndex in range(-1, 2):
+                if rowIndex == 0 and colIndex == 0:
+                    continue
+                if 0 <= (x + colIndex) < self.cols and 0 <= (y + rowIndex) < self.rows:
+                    cell = self.grid[y + rowIndex][x + colIndex]
+                    neighbors += cell
+
+        return neighbors
