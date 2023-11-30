143e39f66d69d615232659914092066f49c6b0d6
StratenyNinja <erik.stvrtecky2000@gmail.com>
2023-11-30 12:17:56 +0100

    Handle mouse click to change a cell and pressing space to stop or resume the game

--- src/game.py
+++ src/game.py
@@ -11,16 +11,26 @@ class Game:
         screen = pygame.display.set_mode((500, 500))
         clock = pygame.time.Clock()
         running = True
+        update = True
 
         while running:
             clock.tick(10)
             screen.fill((0, 0, 0))
             self.grid.draw(screen)
-            self.grid.update()
+            if update:
+                self.grid.update()
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    mouse_pos = pygame.mouse.get_pos()
+                    x, y = mouse_pos[0] // 10, mouse_pos[1] // 10
+                    self.grid.grid[y][x] = 1
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_SPACE:
+                        update = not update
+
 
             pygame.display.update()
 
