aeb6a76746b03a90b698995b5b27061f4dae4dbf
StratenyNinja <erik.stvrtecky2000@gmail.com>
2023-11-30 12:35:20 +0100

    Separate the event handling from the game loop

--- src/game.py
+++ src/game.py
@@ -5,31 +5,43 @@ from grid import Grid
 class Game:
     def __init__(self) -> None:
         self.grid = Grid(50, 50)
+        self.running = True
+        self.update = True
+
+    def handle_events(self):
+        for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+
+                # Change the state of the cell when the user clicks on it
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    mouse_pos = pygame.mouse.get_pos()
+                    x, y = mouse_pos[0] // 10, mouse_pos[1] // 10
+
+                    if self.grid.grid[y][x] == 1:
+                        self.grid.grid[y][x] = 0
+                    else:
+                        self.grid.grid[y][x] = 1
+
+                # Pause or unpause the game when the user presses the space bar
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_SPACE:
+                        self.update = not self.update
 
     def run(self):
         pygame.init()
         screen = pygame.display.set_mode((500, 500))
         clock = pygame.time.Clock()
-        running = True
-        update = True
 
-        while running:
+        while self.running:
             clock.tick(10)
             screen.fill((0, 0, 0))
             self.grid.draw(screen)
-            if update:
+
+            if self.update:
                 self.grid.update()
 
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    running = False
-                if event.type == pygame.MOUSEBUTTONDOWN:
-                    mouse_pos = pygame.mouse.get_pos()
-                    x, y = mouse_pos[0] // 10, mouse_pos[1] // 10
-                    self.grid.grid[y][x] = 1
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_SPACE:
-                        update = not update
+            self.handle_events()
 
 
             pygame.display.update()
