14e030c3ed33bbcec40a8ed5b2377017d1a1c57f
StratenyNinja <erik.stvrtecky2000@gmail.com>
2024-01-11 08:43:22 +0100

    Refactor mouse clicking in toolbar so the grid won't update after click in toolbar

--- src/game.py
+++ src/game.py
@@ -21,15 +21,17 @@ class Game:
             # Change the state of the cell when the user clicks on it
             if event.type == pygame.MOUSEBUTTONDOWN:
                 mouse_pos = pygame.mouse.get_pos()
+                x, y = mouse_pos[0], mouse_pos[1]
 
                 if self.pause_button.is_clicked(mouse_pos) or self.play_button.is_clicked(mouse_pos):
                     self.update = not self.update
 
-                x, y = mouse_pos[0] // 10, (mouse_pos[1]-50) // 10
-                if self.grid.grid[y][x] == 1:
-                    self.grid.grid[y][x] = 0
-                else:
-                    self.grid.grid[y][x] = 1
+                if y >= 50:
+                    x, y = x // 10, (y - 50) // 10
+                    if self.grid.grid[y][x] == 1:
+                        self.grid.grid[y][x] = 0
+                    else:
+                        self.grid.grid[y][x] = 1
 
             # Pause or unpause the game when the user presses the space bar
             if event.type == pygame.KEYDOWN:
