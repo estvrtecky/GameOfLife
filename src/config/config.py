08abe2f0feb15947fcdda0fb10409a450703f38a
StratenyNinja <erik.stvrtecky2000@gmail.com>
2024-03-26 14:36:27 +0100

    Refactor config.py to improve readability

--- src/config/config.py
+++ src/config/config.py
@@ -9,30 +9,27 @@ class Config:
         self.config.read(self.filename)
 
     def get(self, section: str, option: str) -> str:
-        """Get the value of the option in the specified section."""
-
-        return self.config.get(section.upper(), option.lower())
+        """Get an option value for a given section."""
+        return self.config.get(section.upper(), option)
 
     def getint(self, section: str, option: str) -> int:
-        """Get the value of the option in the specified section as an integer."""
-
-        return self.config.getint(section.upper(), option.lower())
+        """Get an option value for a given section as an integer."""
+        return self.config.getint(section.upper(), option)
 
     def set(self, section: str, option: str, value: Union[str, int]) -> None:
         """
-        Set a value to the option in specified section.
+        Set an option value for a given section.
         If the section or option doesn't exist, it will be created.
 
         Changes are written to the config file immediately.
         """
-
         section = section.upper()
         # Create the section if it doesn't exist
         if not self.config.has_section(section):
             self.config.add_section(section)
 
         # Set the option
-        self.config.set(section, option.lower(), str(value))
+        self.config.set(section, option, str(value))
 
         # Write the changes to the config file
         with open(self.filename, "w") as configfile:
